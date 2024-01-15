7d0f71c3c6a249ecc90aab36cff38a6c9702c93d
Jim Lin-Dybner <lin.jycl@gmail.com>
2024-01-15 12:28:29 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Displa String Script Exercise</title>
+</head>
+
+<body>
+    <h1>PHP Displa String Script Exercise</h1>
+    <p>Write a PHP script to display the following strings.</p>
+    <p>
+        Sample String:<br>
+        'Tomorrow I \'ll learn PHP global variables.'<br>
+        'This is a bad command : del c:\\*.*'<br>
+        Expected Output:<br>
+        Tomorrow I 'll learn PHP global variables.<br>
+        This is a bad command : del c:\*.*
+    </p>
+
+    <h2>Actual PHP Output</h2>
+    <?php
+
+    echo 'Tomorrow I\'ll learn PHP global variables.<br>';
+    echo 'This is a bad command : del c:\\*.*';
+
+    ?>
+
+</body>
+
+</html>
